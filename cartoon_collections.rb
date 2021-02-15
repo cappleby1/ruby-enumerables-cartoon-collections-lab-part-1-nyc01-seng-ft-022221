9e37ce46e87fa76a9a2b45c402ac48bcf1db34b1
Colten Appleby <colten96@gmail.com>
2021-02-15 21:23:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |dwarf, index|
-    puts "#{index}. #{dwarf}"
+    puts "#{index+1}. #{dwarf}"
   end 
 end
